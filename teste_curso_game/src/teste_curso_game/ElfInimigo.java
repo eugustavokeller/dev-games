86009e921a0c4d26911f5edb27444feb69037fa7
Gustavo Keller <gustavo.lealtec@gmail.com>
2023-01-08 20:08:57 -0300

    Create ElfInimigo

---
+++ teste_curso_game/src/teste_curso_game/ElfInimigo.java
@@ -0,0 +1,11 @@
+package teste_curso_game;
+
+public class ElfInimigo extends Inimigo {
+
+	public ElfInimigo(int vida) {
+		super(vida);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
--- teste_curso_game/src/teste_curso_game/Inimigo.java
+++ teste_curso_game/src/teste_curso_game/Inimigo.java
@@ -1,5 +1,22 @@
 package teste_curso_game;
 
 public class Inimigo {
+	
+	public int vida = 100;
+	public String inimigoTipo = "tipo1";
+	public int ataqueDano = 10;
+	
+	public Inimigo(int vida) {
+		this.vida = vida;
+	}
+	
+	public int getVida() {
+		return vida;
+	}
+	
+	public void tomarDano() {
+		vida--;
+	}
+	
 
 }
