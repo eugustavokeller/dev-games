17b7c71835e18f51d08463b2d93084ba562294aa
Gustavo Keller <gustavo.lealtec@gmail.com>
2023-01-08 19:41:47 -0300

    loop in Java

--- teste_curso_game/src/teste_curso_game/Game.java
+++ teste_curso_game/src/teste_curso_game/Game.java
@@ -2,6 +2,22 @@ package teste_curso_game;
 
 public class Game {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		// System.out.println("Hello World!");
+		
+		// while ...
+		/*
+		int contador = 0;
+		
+		while(contador <= 10) {
+			System.out.println("O contador esta no while: " + contador);
+			contador++;
+		}
+		*/
+		
+		// for ...
+		int contador = 10;
+		for(int i = 0; i < contador; i++) {
+			System.out.println("O contador esta no for: " + i);
+		}
 	}
 }
