1e5095ce40999fbd23c960dde553f7dcdf7ce98d
Gustavo Keller <gustavo.lealtec@gmail.com>
2023-01-08 21:20:02 -0300

    Array List in java

--- teste_curso_game/src/teste_curso_game/Game.java
+++ teste_curso_game/src/teste_curso_game/Game.java
@@ -1,16 +1,36 @@
 package teste_curso_game;
 
+import java.util.ArrayList;
+
 public class Game {
 	public static void main(String[] args) {
 		
 		
 		Player player = new Player();
+		ArrayList<Inimigo> inimigos = new ArrayList<Inimigo>();
 		
-		ElfInimigo elf = new ElfInimigo(10);
+		inimigos.add(new ElfInimigo(100));
+		inimigos.add(new ElfInimigo(100));
+		inimigos.add(new ElfInimigo(100));
+		inimigos.add(new ElfInimigo(100));
+		inimigos.add(new ElfInimigo(100));
 		
-		elf.tomarDano();
-		
-		System.out.println(elf.getVida());
+		for(int i = 0; i < inimigos.size(); i++) {
+			
+			Inimigo inimigoLocal = inimigos.get(i);
+			
+			if(i == 1) {
+				inimigoLocal.tomarDano();
+				System.out.println(inimigoLocal.vida);
+			} else {
+				System.out.println(inimigoLocal.vida);
+			}
+		}
+//		ElfInimigo elf = new ElfInimigo(10);
+//		
+//		elf.tomarDano();
+//		
+//		System.out.println(elf.getVida());
 		
 //		player.perderVida(2);
 //		System.out.println(player.vida);
